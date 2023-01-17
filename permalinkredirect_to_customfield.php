e2d21207f7352437d7f2b1934b5ce88d096f6204
muteweb <sawan@staginglabs.in>
2023-01-17 16:01:38 +0530

    add some code

--- permalinkredirect_to_customfield.php
+++ permalinkredirect_to_customfield.php
@@ -5,7 +5,7 @@ function my_cpt_template_redirect()
   {
     $id = get_the_id();
     $url=get_field("image_link",$id);
-    if($url) {
+    if($url && $url!="#") {
       wp_redirect( $url );
       die;
     }else{
@@ -16,3 +16,14 @@ function my_cpt_template_redirect()
   }
 }
 add_action( 'template_redirect', 'my_cpt_template_redirect' );
+add_filter('post_type_link', function ($post_link, $post, $leavename, $sample) {
+  if ($post->post_type == 'websites') {
+        $id = $post->ID;
+        $url=get_field("image_link",$id);
+        if($url) { $post_link=$url; }else{ $post_link="javascript:void(0);"; } 
+  }
+  return $post_link;
+}, 999, 4);
+
+
+
